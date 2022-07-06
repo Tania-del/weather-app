a896f480a5d3677d59000832d65a9bb4fbc997ae
Tania-del <78879254+Tania-del@users.noreply.github.com>
2022-07-06 18:33:03 +0300

    Update search engine

--- src/index.js
+++ src/index.js
@@ -1,41 +1,19 @@
 import * as axios from "axios";
-// let weather = {
-//   paris: {
-//     temp: 19.7,
-//     humidity: 80
-//   },
-//   tokyo: {
-//     temp: 17.3,
-//     humidity: 50
-//   },
-//   lisbon: {
-//     temp: 30.2,
-//     humidity: 20
-//   },
-//   "san francisco": {
-//     temp: 20.9,
-//     humidity: 100
-//   },
-//   moscow: {
-//     temp: -5,
-//     humidity: 20
-//   }
-// };
-// let city = prompt("Enter a city");
-// city = city?.toLowerCase();
 
-// if (weather[city] !== undefined) {
-//   let temperature = Math.floor(weather[city].temp);
-//   let humidity = weather[city].humidity;
+const tempEl = document.querySelector(".temp");
 
-//   alert(
-//     `It is currently ${temperature}°C in ${city} with a humidity of ${humidity}%`
-//   );
-// } else {
-//   alert(
-//     "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney"
-//   );
-// }
+const dayEl = document.querySelector("#day");
+const timeEl = document.querySelector("#time");
+const cityEl = document.querySelector("#city");
+const precipitationEl = document.querySelector("#precipitation");
+const windEl = document.querySelector("#wind");
+const descriptionEl = document.querySelector("#description");
+const iconEl = document.querySelector("#icon");
+const formEl = document.querySelector("#form");
+const celciumEl = document.querySelector("#celcium");
+const fahrenheitEl = document.querySelector("#fahrenheit");
+const currentTownEl = document.querySelector("#town-list");
+const currentButtonEl = document.querySelector("#current-button");
 
 const getCurrentDay = () => {
   const now = new Date();
@@ -50,38 +28,23 @@ const getCurrentDay = () => {
   ];
 
   const day = now.getDay();
-
   return `${days[day]}`;
 };
 
 const getCurrentTime = () => {
   const now = new Date();
-
   const hour = now.getHours().toString().padStart(2, "0");
   const min = now.getMinutes().toString().padStart(2, "0");
-
   return `${hour}:${min}`;
 };
-
-let dayEl = document.querySelector("#day");
-let timeEl = document.querySelector("#time");
-
-const tempEl = document.querySelector(".temp");
-const cityEl = document.querySelector("#city");
-const precipitationEl = document.querySelector("#precipitation");
-const windEl = document.querySelector("#wind");
-const descriptionEl = document.querySelector("#description");
-const iconEl = document.querySelector("#icon");
 
 const updateData = (response) => {
   const { temp, humidity } = response.data.main;
   console.log(response);
   const { speed } = response.data.wind;
   const { description } = response.data.weather[0];
-
   dayEl.textContent = getCurrentDay();
   timeEl.textContent = getCurrentTime();
-
   tempEl.textContent = Math.round(temp);
   cityEl.textContent = response.data.name;
   precipitationEl.textContent = humidity;
@@ -93,38 +56,47 @@ const updateData = (response) => {
   );
 };
 
-const searchCity = (event) => {
-  event.preventDefault();
-  const formInputEl = document.querySelector("#form-input").value;
-  let units = "metric";
-  let apiKey = "ff302840c34e328a290e351206228b12";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${formInputEl}&units=${units}`;
+const getWeatherByCity = (city) => {
+  const units = "metric";
+  const apiKey = "ff302840c34e328a290e351206228b12";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=${units}`;
   axios.get(`${apiUrl}`).then(updateData);
 };
-let formEl = document.querySelector("#form");
+
+const searchCity = (event) => {
+  event.preventDefault();
+  const city = document.querySelector("#form-input").value;
+  getWeatherByCity(city);
+};
+
 formEl.addEventListener("submit", searchCity);
 
-const getPosition = ({ coords }) => {
-  const { latitude, longitude } = coords;
+const getTownEl = (event) => {
+  event.preventDefault();
+  const city = event.target.textContent;
+  getWeatherByCity(city);
+};
+
+currentTownEl.addEventListener("click", getTownEl);
+
+const getWetherByCoords = ({ latitude, longitude }) => {
   let units = "metric";
   let apiKey = "ff302840c34e328a290e351206228b12";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
   axios.get(`${apiUrl}`).then(updateData);
 };
-
-const getCurrentPosition = () => {
-  navigator.geolocation.getCurrentPosition(getPosition);
+const getCoords = ({ coords }) => {
+  getWetherByCoords(coords);
 };
 
-let currentButtonEl = document.querySelector("#current-button");
+const getCurrentPosition = () => {
+  navigator.geolocation.getCurrentPosition(getCoords);
+};
+
 currentButtonEl.addEventListener("click", getCurrentPosition);
 
 const convertToFahrenheit = (celcium) => {
   return Math.round(celcium * 1.8 + 32);
-};
-
-const convertToCelcium = (fahrenheit) => {
-  return Math.round((fahrenheit - 32) / 1.8);
 };
 
 const onClickFahrenheit = () => {
@@ -133,6 +105,13 @@ const onClickFahrenheit = () => {
     tempEl.textContent = convertToFahrenheit(celcium);
   }
 };
+
+fahrenheitEl.addEventListener("click", onClickFahrenheit);
+
+const convertToCelcium = (fahrenheit) => {
+  return Math.round((fahrenheit - 32) / 1.8);
+};
+
 const onClickCelcium = () => {
   const fahrenheit = tempEl.textContent;
 
@@ -141,7 +120,4 @@ const onClickCelcium = () => {
   }
 };
 
-const celciumEl = document.querySelector("#celcium");
-const fahrenheitEl = document.querySelector("#fahrenheit");
 celciumEl.addEventListener("click", onClickCelcium);
-fahrenheitEl.addEventListener("click", onClickFahrenheit);
